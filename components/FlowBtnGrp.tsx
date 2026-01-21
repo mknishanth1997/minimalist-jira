0c2d3a31aa8c158de29c725ecce3956b66f76147
nishanth <mksabarinishanth@gmail.com>
2026-01-22 03:10:57 +0530

    Filter button setup finished and it's working

--- components/FlowBtnGrp.tsx
+++ components/FlowBtnGrp.tsx
@@ -2,22 +2,34 @@ import { useTask } from "@/context/TaskContext";
 import { Button, ButtonGroup } from "flowbite-react";
 
 export function FlowBtnGrp() {
-  const { state } = useTask();
+  const { state, dispatch } = useTask();
+  const setCurrentFilter = (f) =>
+    dispatch({ type: "SET_UI_FILTER", payload: f });
   const className = "bg-gray-100 dark:bg-gray-700";
   return (
     <ButtonGroup outline>
-      <Button className={`${state.currentFilter === "all" && className}`}>
+      <Button
+        className={`${state.currentFilter === "all" && className}`}
+        onClick={() => setCurrentFilter("all")}
+      >
         All
       </Button>
-      <Button className={`${state.currentFilter === "todo" && className}`}>
+      <Button
+        className={`${state.currentFilter === "todo" && className}`}
+        onClick={() => setCurrentFilter("todo")}
+      >
         Todo
       </Button>
       <Button
         className={`${state.currentFilter === "in_progress" && className}`}
+        onClick={() => setCurrentFilter("in_progress")}
       >
         In-Progress
       </Button>
-      <Button className={`${state.currentFilter === "done" && className}`}>
+      <Button
+        className={`${state.currentFilter === "done" && className}`}
+        onClick={() => setCurrentFilter("done")}
+      >
         Done
       </Button>
     </ButtonGroup>
